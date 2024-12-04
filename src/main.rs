74dbb7a2f9a582232b79c017c9ae8d2631657a85
CenTdemeern1 <timo.herngreen@gmail.com>
2024-12-04 13:58:40 +0100

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, io::{Read, Write}, net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpStream}, time::Duration};
+use std::{collections::HashSet, io::{Read, Write}, net::TcpStream};
 
 use ctru::{applets::swkbd::{self, Button, ButtonConfig, Features, SoftwareKeyboard}, prelude::*};
 use serde::Serialize;
@@ -27,7 +27,8 @@ enum ReactionAcceptance {
 #[serde(rename_all = "camelCase")]
 struct Post {
     visibility: Visibility,
-    visible_user_ids: HashSet<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    visible_user_ids: Option<HashSet<String>>,
     cw: Option<String>,
     local_only: bool,
     reaction_acceptance: Option<ReactionAcceptance>,
@@ -38,59 +39,26 @@ struct Post {
     renote_id: Option<String>,
     channel_id: Option<String>,
     text: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     file_ids: Option<HashSet<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     media_ids: Option<HashSet<String>>,
     poll: Option<bool>,
 }
 
 fn create_post(post: &Post) -> String {
     let json = serde_json::to_string_pretty(post).unwrap();
-    format!("POST /api/notes/create HTTP/1.1
-Content-Type: application/json
-User-Agent: misskey-3ds
-Authorization: Bearer {TOKEN}
-Host: eepy.moe
-Content-Length: {}
-
+    format!("POST /api/notes/create HTTP/1.1\r
+Content-Type: application/json\r
+User-Agent: misskey-3ds\r
+Authorization: Bearer {TOKEN}\r
+Host: eepy.moe\r
+Content-Length: {}\r
+\r
 {json}", json.len())
 }
 
-fn main() {
-    let apt = Apt::new().unwrap();
-    let mut hid = Hid::new().unwrap();
-    let gfx = Gfx::new().unwrap();
-    let _console = Console::new(gfx.top_screen.borrow_mut());
-    let mut sockets = Soc::new().unwrap();
-    sockets.redirect_to_3dslink(true, true).unwrap();
-
-    let mut keyboard = SoftwareKeyboard::new(swkbd::Kind::Normal, ButtonConfig::LeftRight);
-    keyboard.configure_button(Button::Left, "Cancel", false);
-    keyboard.configure_button(Button::Right, "Submit", true);
-    keyboard.set_features(Features::MULTILINE | Features::PREDICTIVE_INPUT);
-    
-    if let Ok((text, Button::Right)) = keyboard.launch(&apt, &gfx) {
-        let post = create_post(&Post {
-            visibility: Visibility::Followers,
-            text: Some(text),
-            ..Default::default()
-        });
-        println!("{}", post);
-        let tcp_stream = TcpStream::connect(
-            "5.161.73.220:80"
-        );
-        match tcp_stream {
-            Ok(mut tcp) => {
-                tcp.write_all(post.as_bytes()).unwrap();
-                tcp.flush().unwrap();
-                tcp.shutdown(std::net::Shutdown::Write).unwrap();
-                let mut s = String::new();
-                tcp.read_to_string(&mut s).unwrap();
-                println!("{}", s);
-            },
-            Err(err) => println!("Didn't work, fuck. {err}")
-        };
-    }
-
+fn wait_to_exit(apt: &Apt, gfx: &Gfx, hid: &mut Hid) {
     println!("\x1b[29;16HPress Start to exit");
 
     while apt.main_loop() {
@@ -101,4 +69,44 @@ fn main() {
             break;
         }
     }
+}
+
+fn main() {
+    let apt = Apt::new().unwrap();
+    let mut hid = Hid::new().unwrap();
+    let gfx = Gfx::new().unwrap();
+    let _console = Console::new(gfx.top_screen.borrow_mut());
+    let mut sockets = Soc::new().unwrap();
+    // sockets.redirect_to_3dslink(true, true).unwrap();
+
+    let mut keyboard = SoftwareKeyboard::new(swkbd::Kind::Normal, ButtonConfig::LeftRight);
+    keyboard.configure_button(Button::Left, "Cancel", false);
+    keyboard.configure_button(Button::Right, "Submit", true);
+    keyboard.set_features(Features::MULTILINE | Features::PREDICTIVE_INPUT);
+
+    let mut tcp = match TcpStream::connect("192.168.178.89:80") {
+        Ok(tcp) => tcp,
+        Err(err) => {
+            eprintln!("Couldn't connect: {err}");
+            wait_to_exit(&apt, &gfx, &mut hid);
+            return;
+        }
+    };
+    
+    while let Ok((text, Button::Right)) = keyboard.launch(&apt, &gfx) {
+        let post = create_post(&Post {
+            visibility: Visibility::Public,
+            text: Some(text),
+            ..Default::default()
+        });
+        println!("{}", post);
+        tcp.write_all(post.as_bytes()).unwrap();
+        tcp.flush().unwrap();
+        // tcp.shutdown(std::net::Shutdown::Write).unwrap();
+        // let mut s = String::new();
+        // tcp.read_to_string(&mut s).unwrap();
+        // println!("{}", s);
+    }
+
+    wait_to_exit(&apt, &gfx, &mut hid);
 }
